29521e5bc0fd8a1e4c0b323424fad0dd9f9d2ded
gbelot2003 <gbelot2003@hotmail.com>
2015-03-30 15:51:13 -0600

    configurada logica de login cuando usuario ya esta logueado

--- default/app/controllers/index_controller.php
+++ default/app/controllers/index_controller.php
@@ -21,18 +21,22 @@ class IndexController extends AppController
 
 	public function login()
 	{
-		$this->navActive['login'] = "active";
+		if(!Auth::is_valid()){
+			$this->navActive['login'] = "active";
+			if(Input::hasPost('user', 'password')){
+				$pdw = sha1(Input::post('password'));
+				$user = Input::post('user');
 
-		if(Input::hasPost('user', 'password')){
-			$pdw = sha1(Input::post('password'));
-			$user = Input::post('user');
-
-			$auth = new Auth("model", "class: user", "user: $user", "password: $pdw");
-			if($auth->authenticate()){
-				Redirect::to('index');
-			} else {
-				Flash::error('Ha habido un error en tu login, por favor verifica tu usuario y password');
+				$auth = new Auth("model", "class: user", "user: $user", "password: $pdw");
+				if($auth->authenticate()){
+					Redirect::to('index');
+				} else {
+					Flash::error('Ha habido un error en tu login, por favor verifica tu usuario y password');
+				}
 			}
+		} else {
+			Flash::warning('Ya estas logueado');
+			Redirect::toAction('');
 		}
 	}
 
