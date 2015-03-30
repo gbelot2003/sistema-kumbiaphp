c9b9a65e1014c2093bba03897b30e834d4ee16cf
gbelot2003 <gbelot2003@hotmail.com>
2015-03-30 16:15:55 -0600

    creado esqueleto de CRUD para administración de roles

---
+++ default/app/controllers/rol_controller.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Controlador Rol
+ * 
+ * @category App
+ * @package Controllers
+ */
+class RolController extends AdminController
+{
+	public function index()
+	{
+
+	}
+
+	public function create()
+	{
+
+	}
+
+	public function edit()
+	{
+
+	}
+
+	public function delete()
+	{
+
+	}
+}
